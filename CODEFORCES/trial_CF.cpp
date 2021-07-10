6ce2f3f960f71b60692ad4c68dc04de7258caa30
Jaagrit Arora <jaagritarora.btcse20@pec.edu.in>
2021-07-11 02:37:46 +0530

    Create trial_CF.cpp

---
+++ CODEFORCES/trial_CF.cpp
@@ -0,0 +1,15 @@
+// TEMPLATE_JAAGRIT_ARORA
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int 
+#define flash ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define forloop_n for(ll i=0; i<n; i++)
+#define And &&
+#define Or ||
+int main()
+
+
+{
+    flash
+    return 0;
+}
