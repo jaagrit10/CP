74bf4505d6c431d6bc9d5ae390259f6aa5d3cd5b
Jaagrit Arora <jaagritarora.btcse20@pec.edu.in>
2021-10-01 22:09:44 +0530

    C

--- CODEFORCES/ticks_CF.cpp
+++ CODEFORCES/ticks_CF.cpp
@@ -39,7 +39,7 @@ int main()
         }
         }
         forloop_n
-        {
+        // {
         // for (ll j = 0; j<m; j++)
         // {
         //     cout << arr[i][j];
@@ -49,7 +49,7 @@ int main()
 
         for(ll i = n-1; i>=n-k; i--)
         {
-            for ( ll j = 0; j<k-n-1-i; j++)
+            for ( ll j = 0; j<k-(n-1-i); j++)
             {
                 // cout << arr[i][j];
                 if (arr[i][j]=='*') 
@@ -62,26 +62,136 @@ int main()
 
             for (ll p = m-1; p>=m-(k-(n-1-i));p--)
             {
-                cout << i << " "<< p <<endl;
+                // cout << i << " "<< p <<endl;
                 if (arr[i][p]=='*')
                 {
                     ans = false;
                 }
             }
-            cout << endl;
+            // cout << endl;
 
         }
-        cout << arr[4][6] << endl;
+        bool smalltick = false;
 
 
+        for(ll i = n-1; i>=k; i--)
+        {
+            for (ll j = k; j<m-k; j++)
+            {
+                if (arr[i][j]=='*')
+                {
+                    // cout << i << " " << j << endl;
+                    // visited[i][j]=true;
+                    ll count = 0;
+                    smalltick = false;
+                    for (ll tick = 1; tick<=min(j,m-j-1); tick++)
+                    {
+                        if (i-tick>=0 And j+tick<m And j-tick>=0)
+                        // cout << i-tick << " " << j+tick << "  " << i-tick << " " << j-tick << endl;
+                        {    if (arr[i-tick][j+tick]=='*' And arr[i-tick][j-tick]=='*')
+                            {
+                                // cout << i-tick << " " << j+tick << "  " << i-tick << " " << j-tick << endl;
+                                count++;
+                                visited[i-tick][j+tick] = true;
+                                visited[i-tick][j-tick] = true;
+                                
+                            }}
+
+                    }
+
+
+                    if (count<k And visited[i][j]!=true)
+                    {
+                        smalltick = true;
+                        break;
+                    }
+                    visited[i][j] = true;
+                }
+            }
+
+
+            if (smalltick)
+            {
+                break;
+            }
 
         }
 
-        if (!ans)
+
+        bool visits = true;
+
+        // forloop_n
+        // {
+        //     for (ll j = 0; j<m; j++)
+        //     {
+        //         cout << visited[i][j];
+        //     }
+        //     cout << endl;
+        // }
+
+        
+
+
+        
+
+
+        if (smalltick)
+        {
+            cout << "NO" << "\n";
+        }
+
+
+
+
+        else if (!ans)
         {
             cout << "NO" <<"\n";
             
         }
+
+        
+
+        else if (smalltick==false And ans==true )
+        {
+            for (ll i =0; i<n; i++)
+            {
+                for (ll j = 0; j<m; j++)
+                {
+                    if (arr[i][j]=='*' And visited[i][j]==false)
+                    {
+                        visits = false;
+                        break;
+
+                    }
+                }
+
+                if (!visits)
+                {
+                    break;
+                }
+            }
+
+
+            if (!visits)
+            {
+                cout << "NO" << endl;
+            }
+
+            else
+            {
+                cout << "YES" <<endl;
+            }
+
+        }
+
+        
+
+        // else
+        // {
+        //     cout << "YES" << endl;
+        // }
+        // cout << smalltick <<" " << ans << " " << visits << endl;
+        
 
         
 
